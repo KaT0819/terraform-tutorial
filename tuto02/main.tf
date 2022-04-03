3c86fc655c1d380ea3aa04f13ea5a7cc5d24e996
Katsuyuki Toki <katuyuki.toki@gmail.com>
2022-04-04 01:20:41 +0900

    変数の定義化 Fixes #3

--- tuto02/main.tf
+++ tuto02/main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("credential.json")
+  credentials = file(var.credentials_file)
 
-  project = "{プロジェクトID}"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
